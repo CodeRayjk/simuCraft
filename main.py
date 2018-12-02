8467c0b85890c63c3542c4aa4d39ddc4cd272a16
Rayjk <jacob.m.wahlstrom@gmail.com>
2018-12-02 21:34:38 +0100

    [Task] Messing around What: Added some logging in main and args Where: main.py

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 
 
-if __name__ == '__main__':
-    pass
+import argparse
+import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(funcName)s -  %(levelname)s - %(message)s')
+
+
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="simuCraft the best wow simu ever!")
+    parser.add_argument("-g", "--gui", action="store_true",
+                    help="Activates GUI")
+    parser.add_argument("-i", "--iterations", help="Number of iterations to run in simulation")
+    parser.add_argument("-p", "--player", help="Player config")
+    parser.add_argument("-e", "--enemy", help="Enemy config")
+
+    return parser.parse_args()
+
+def run(args):
+
+
+    logging.debug('INSIDE RUN')
+
+
+
+    return 0
+
+if __name__ == '__main__':
+
+    logging.debug('INSIDE MAIN')
+    args = parse()
+    run(args)
