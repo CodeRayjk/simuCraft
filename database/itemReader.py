98850fda137bf7bb71b325ca6df816b17e4900b8
Rayjk <jacob.m.wahlstrom@gmail.com>
2018-12-04 22:33:03 +0100

    [Task] itemReader What: itemReader from cvs "DB" Where: database/itemReader        database/item_template.csv

---
+++ database/itemReader.py
@@ -0,0 +1,119 @@
+
+import csv
+''''
+"2825","2","2","Bow of Searing Arrows","20552","4","0","1","73609","14721","15","-1","-1","42","37","0","0","0","0","0","0","0","0","1","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","47","88","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","2700","2","100","29624","1","0","0","-1","0","-1","0","0","0","0","-1","0","-1","0","0","0","0","-1","0","-1","0","0","0","0","-1","0","-1","0","0","0","0","0","0","-1","2","","0","0","0","0","0","2","0","0","0","0","90","0","0","0","","61","0","0","0","0","0"
+"22465","4","3","Earthshatter Legguards","35754","4","0","1","763444","152688","7","64","255","88","60","0","0","0","0","0","0","0","0","1","0","7","28","5","30","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","484","0","0","0","0","0","0","0","0","0","18038","1","0","0","-1","0","-1","21365","1","0","0","-1","0","-1","0","0","0","0","-1","0","-1","0","0","0","0","-1","0","-1","0","0","0","0","0","0","0","1","","0","0","0","0","0","5","0","0","0","527","105","0","0","0","","65","0","0","0","0","0"
+
+"entry",\
+"class",
+"subclass",
+"name",
+"displayid",
+"Quality",
+"Flags",
+"BuyCount",
+"BuyPrice",
+"SellPrice",
+"InventoryType",
+"AllowableClass",
+"AllowableRace",
+"ItemLevel",
+"RequiredLevel",
+"RequiredSkill",
+"RequiredSkillRank",
+"requiredspell",
+"requiredhonorrank",
+"RequiredCityRank",
+"RequiredReputationFaction",
+"RequiredReputationRank",
+"maxcount",
+"stackable",
+"ContainerSlots",
+stat_types: 1 - X  :  2 - X  : 3 - Agility : 4 - Strength : 5 - Intellect : 6 - Spirit : 7 - Stamina : 8 - X : 
+
+"stat_type1","stat_value1",   
+"stat_type2","stat_value2",
+"stat_type3","stat_value3",
+"stat_type4","stat_value4",
+"stat_type5","stat_value5",
+"stat_type6","stat_value6",
+"stat_type7","stat_value7",
+"stat_type8","stat_value8",
+"stat_type9","stat_value9",
+"stat_type10","stat_value10",
+"dmg_min1","dmg_max1","dmg_type1",
+"dmg_min2","dmg_max2","dmg_type2",
+"dmg_min3","dmg_max3","dmg_type3",
+"dmg_min4","dmg_max4","dmg_type4",
+"dmg_min5","dmg_max5","dmg_type5",
+"armor",
+"holy_res","fire_res","nature_res","frost_res","shadow_res","arcane_res",
+"delay","ammo_type","RangedModRange",
+"spellid_1","spelltrigger_1","spellcharges_1","spellppmRate_1",
+"spellcooldown_1","spellcategory_1","spellcategorycooldown_1",
+"spellid_2","spelltrigger_2","spellcharges_2","spellppmRate_2",
+"spellcooldown_2","spellcategory_2","spellcategorycooldown_2",
+"spellid_3","spelltrigger_3","spellcharges_3","spellppmRate_3",
+"spellcooldown_3","spellcategory_3","spellcategorycooldown_3",
+"spellid_4","spelltrigger_4","spellcharges_4","spellppmRate_4",
+"spellcooldown_4","spellcategory_4","spellcategorycooldown_4",
+"spellid_5","spelltrigger_5","spellcharges_5","spellppmRate_5",
+"spellcooldown_5","spellcategory_5","spellcategorycooldown_5",
+"bonding",
+"description",
+"PageText",
+"LanguageID",
+"PageMaterial",
+"startquest",
+"lockid",
+"Material",
+"sheath",
+"RandomProperty",
+"block",
+"itemset",
+"MaxDurability",
+"area",
+"Map",
+"BagFamily",
+"ScriptName",
+"DisenchantID",
+"FoodType",
+"minMoneyLoot",
+"maxMoneyLoot",
+"Duration",
+"ExtraFlags"
+
+'''
+
+attributeConv = {'3': 'Agility' , '4':'Strength', '5' : 'Intellect', '6' : 'Spirit', '7' : 'Stamina'}
+
+AllItem = csv.reader(open('C:\\Users\\Rayjk\PycharmProjects\simuCraft\database\item_template.csv'),delimiter = ',')
+first = True
+firstRow = None
+for row in AllItem:
+
+    if first:
+        firstRow = row
+        #print(firstRow)
+        first = False
+
+    for item in row:
+        if 'Might of' in item:
+            #print(row)
+            print(row[firstRow.index('name')])
+            if row[firstRow.index('armor')] is not '0':
+                print( 'Armor : %s' % row[firstRow.index('armor')])
+
+            for i in range(1,6):
+                if row[firstRow.index('stat_type%d' % i)] is not '0':
+                    print('%s : %s' % (attributeConv[row[firstRow.index('stat_type%d' % i)]], row[firstRow.index('stat_value%d' % i)]))
+
+            print('---------------------')
+
+            #print( '%s : %s' % (attributeConv[row[firstRow.index('stat_type1')]],row[firstRow.index('stat_value1')]))
+            #print('%s : %s' % (attributeConv[row[firstRow.index('stat_type2')]], row[firstRow.index('stat_value2')]))
+            #print('%s : %s' % (attributeConv[row[firstRow.index('stat_type3')]], row[firstRow.index('stat_value3')]))
+
+
+
+
